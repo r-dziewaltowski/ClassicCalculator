60d04c72df2dd650f5e5bf8b9d443a8e6826b3b1
Rafal Dziewaltowski-Gintowt <135457365+r-dziewaltowski@users.noreply.github.com>
2024-11-27 19:56:56 +0000

    Delete ClassicCalculatorWebApp directory

--- ClassicCalculatorWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClassicCalculatorWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
