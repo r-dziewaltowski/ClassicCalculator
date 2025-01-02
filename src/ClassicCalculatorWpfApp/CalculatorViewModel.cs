5e3e194d28e56a4136d03d6741f5ec66c03c7b33
r-dziewaltowski <135457365+r-dziewaltowski@users.noreply.github.com>
2025-01-02 20:23:29 +0000

    Use source generator attributes

--- src/ClassicCalculatorWpfApp/CalculatorViewModel.cs
+++ src/ClassicCalculatorWpfApp/CalculatorViewModel.cs
@@ -1,24 +1,17 @@
 ﻿using ClassicCalculator;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System.Windows.Input;
 
 namespace ClassicCalculatorWpfApp
 {
-    public class CalculatorViewModel : ObservableObject
+    public partial class CalculatorViewModel(ICalculator calculator) : ObservableObject
     {
         public string DisplayValue => _calculator.DisplayValue;
 
-        public ICommand PressButtonCommand { get; }
 
-        private readonly ICalculator _calculator;
+        private readonly ICalculator _calculator = calculator;
 
-        public CalculatorViewModel(ICalculator calculator)
-        {
-            _calculator = calculator;
-            PressButtonCommand = new RelayCommand<CalculatorButton>(PressButton);
-        }
-
+        [RelayCommand]
         private void PressButton(CalculatorButton button)
         {
             _calculator.PressButton(button);
