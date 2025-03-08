e2d3608e54c8e03f1b90f93fec2cb2d38e12a132
r-dziewaltowski <135457365+r-dziewaltowski@users.noreply.github.com>
2025-03-08 20:41:22 +0000

    Parser

---
+++ tests/ClassicCalculator.Tests/Parser/Parser.cs
@@ -0,0 +1,68 @@
+﻿namespace ClassicCalculator.Tests.Parser
+{
+    public static class Parser
+    {
+        private static readonly Dictionary<string, CalculatorButton> _buttonMap = new()
+        {
+            { "C", CalculatorButton.Clear },
+            { "%", CalculatorButton.Percentage },
+            { "SR", CalculatorButton.SquareRoot },
+            { "*", CalculatorButton.Multiply },
+            { "-", CalculatorButton.Subtract },
+            { "+", CalculatorButton.Add },
+            { "/", CalculatorButton.Divide },
+            { "+/-", CalculatorButton.ToggleSign },
+            { ".", CalculatorButton.Decimal },
+            { "=", CalculatorButton.Equals }
+        };
+
+        public static List<CalculatorButton> ParseInputToButtonSequence(string input)
+        {
+            var tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var result = new List<CalculatorButton>();
+
+            foreach (var token in tokens)
+            {
+                if (decimal.TryParse(token, out _))
+                {
+                    result.AddRange(ParseNumberToButtonSequence(token));
+                }
+                else
+                {
+                    result.Add(_buttonMap[token]);
+                }
+            }
+
+            return result;
+        }
+
+        private static List<CalculatorButton> ParseNumberToButtonSequence(string number)
+        {
+            var negativeNumber = false;
+            var result = new List<CalculatorButton>();
+
+            foreach (var digit in number)
+            {
+                switch (digit)
+                {
+                    case '-':
+                        negativeNumber = true;
+                        break;
+                    case '.':
+                        result.Add(CalculatorButton.Decimal);
+                        break;
+                    default:
+                        result.Add((CalculatorButton)int.Parse(digit.ToString()));
+                        break;
+                }
+            }
+
+            if (negativeNumber)
+            {
+                result.Add(CalculatorButton.ToggleSign);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/ClassicCalculator.Tests/Parser/ParserTests.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassicCalculator.Tests.Parser
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void ParseInputToButtonSequence_ShouldReturnCorrectResult()
+        {
+            // Act
+            const string input = "-1.2 + - * / . +/- % C SR =";
+            var result = Parser.ParseInputToButtonSequence(input);
+            
+            // Assert
+            var expectedResult = new List<CalculatorButton>
+            {
+                CalculatorButton.One,
+                CalculatorButton.Decimal,
+                CalculatorButton.Two,
+                CalculatorButton.ToggleSign,
+                CalculatorButton.Add,
+                CalculatorButton.Subtract,
+                CalculatorButton.Multiply,
+                CalculatorButton.Divide,
+                CalculatorButton.Decimal,
+                CalculatorButton.ToggleSign,
+                CalculatorButton.Percentage,
+                CalculatorButton.Clear,
+                CalculatorButton.SquareRoot,
+                CalculatorButton.Equals
+            };
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
